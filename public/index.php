53f2fca092d1d8f2621200ce38cc398cb1f741d3
sytnic <sytnic@list.ru>
2024-07-01 13:45:04 +0300

    06-Include and require files

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Globe Bank</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+    <header>
+      <h1></h1>
+    </header>
+
+
+
+
+  </body>
+</html>
