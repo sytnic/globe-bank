41f87065ef3c6e70e3ebec4046962243dd1abce0
sytnic <sytnic@list.ru>
2024-07-01 16:55:27 +0300

    08.2 Links and URLs

--- private/functions.php
+++ private/functions.php
@@ -1,3 +1,14 @@
 <?php
 
+// Функция создаёт url-путь от корня сайта для указываемого пути
+function url_for($script_path) {
+    // add the leading '/' if not present
+    // добавит лидирующий слэш, если его не было
+    if($script_path[0] != '/') {
+      $script_path = "/" . $script_path;
+    }
+
+    return WWW_ROOT . $script_path;
+}
+
 ?>
