61552d730c7f27eb019a38f7034b5a05c7c855b8
sytnic <sytnic@list.ru>
2024-09-23 16:57:22 +0300

    11. Encode URL parameters

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,3 +1,4 @@
+<?php require_once('../../../private/initialize.php'); ?>
 <?php
 
 // классический вариант проверки наличия значения
@@ -18,3 +19,7 @@ $id = $_GET['id'] ?? '1';  // for PHP > 7.0
 echo $id;
 
 ?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
