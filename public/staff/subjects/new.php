88848ecb7614277533ef1275de83712c34aea3c0
sytnic <sytnic@list.ru>
2024-10-14 15:14:27 +0300

    16.0 Page redirection

--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -8,6 +8,9 @@ if($test == '404') {
   error_404();
 } elseif($test == '500') {
   error_500();
+} elseif($test == 'redirect') {
+  header("Location: ".url_for('/staff/subjects/index.php'));
+  exit;
 } else {
   echo 'No error';
 }
