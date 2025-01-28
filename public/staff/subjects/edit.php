a3d9b2236d788adb0c33294a121816aed79f5d98
sytnic <sytnic@list.ru>
2025-01-28 16:31:31 +0300

    18. Build forms. New and Edit forms

--- public/staff/subjects/edit.php
+++ public/staff/subjects/edit.php
@@ -13,15 +13,15 @@ if($test == '404') {
 }
 ?>
 
-<?php $page_title = 'Create Subject'; ?>
+<?php $page_title = 'Edit Subject'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
 
   <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
 
-  <div class="subject new">
-    <h1>Create Subject</h1>
+  <div class="subject edit">
+    <h1>Edit Subject</h1>
 
     <form action="" method="post">
       <dl>
@@ -44,7 +44,7 @@ if($test == '404') {
         </dd>
       </dl>
       <div id="operations">
-        <input type="submit" value="Create Subject" />
+        <input type="submit" value="Edit Subject" />
       </div>
     </form>
 
