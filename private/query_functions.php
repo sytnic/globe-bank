de63d8c6740418156923434dd38a7004997e5334
sytnic <sytnic@list.ru>
2025-03-19 15:39:10 +0300

    36. Error handling

--- private/query_functions.php
+++ private/query_functions.php
@@ -6,6 +6,7 @@ function find_all_subjects() {
     $sql = "SELECT * FROM subjects";
     $sql.= " ORDER BY position ASC";
     $result_set = mysqli_query($db, $sql);
+    confirm_result_set($result_set);
     return $result_set;
 }
 
