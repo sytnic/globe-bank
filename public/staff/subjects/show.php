e1f8058d2e57ec37f11b5270f4292925fb912fcf
sytnic <sytnic@list.ru>
2025-03-24 14:59:24 +0300

    39.0 Find a single record

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -16,6 +16,14 @@ if(isset($_GET['id'])) {
 // 3 вариант - оператор нулевого слияния
 $id = $_GET['id'] ?? '1';  // for PHP > 7.0
 
+$sql = "SELECT * FROM subjects";
+$sql.= " WHERE id='".$id."'";
+$result = mysqli_query($db, $sql);
+confirm_result_set($result);
+
+$subject = mysqli_fetch_assoc($result);
+mysqli_free_result($result);
+
 ?>
 
 <?php $page_title = 'Show Subject'; ?>
@@ -27,10 +35,23 @@ $id = $_GET['id'] ?? '1';  // for PHP > 7.0
 
     <div class="subject show">
 
-    Subject ID: <?php echo h($id); ?>
+        <h1>Subject: <?php echo h($subject['menu_name']); ?></h1>
 
+        <div class="attributes">
+            <dl>
+                <dt>Menu Name</dt>
+                <dd><?php echo h($subject['menu_name']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Position</dt>
+                <dd><?php echo h($subject['position']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Visible</dt>
+                <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
+            </dl>
+        </div>
     </div>
-
 </div>
 
 <?php include(SHARED_PATH.'/staff_footer.php');  ?>
