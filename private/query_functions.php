edbc24911e41c614525a1b72d03e38c83df872fa
sytnic <sytnic@list.ru>
2025-03-24 14:18:36 +0300

    38. Solution List pages

--- private/query_functions.php
+++ private/query_functions.php
@@ -10,5 +10,15 @@ function find_all_subjects() {
     return $result_set;
 }
 
+function find_all_pages() {
+    global $db;
+    
+    $sql = "SELECT * FROM pages";
+    $sql.= " ORDER BY subject_id ASC, position ASC";
+    $result_set = mysqli_query($db, $sql);
+    confirm_result_set($result_set);
+    return $result_set;
+}
+
 
 ?>
--- public/staff/pages/index.php
+++ public/staff/pages/index.php
@@ -1,12 +1,15 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
 <?php
-  $pages = [
+//теперь вместо этого массива будет результат, полученный из БД
+/*  $pages = [
     ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'Globe Bank'],
     ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'History'],
     ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'Leadership'],
     ['id' => '4', 'position' => '4', 'visible' => '1', 'menu_name' => 'Contact Us'],
   ];
+*/
+  $page_set = find_all_pages();
 ?>
 
 <?php $page_title = 'Pages'; ?>
@@ -23,6 +26,7 @@
         <table class="list">
           <tr>
               <th>ID</th>
+              <th>Subject ID</th>
               <th>Position</th>
               <th>Visible</th>
               <th>Name</th>
@@ -31,9 +35,13 @@
               <th>&nbsp;</th>
           </tr>
 
-          <?php foreach($pages as $page) { ?>
+          <?php 
+              //foreach($pages as $page) { 
+              while($page = mysqli_fetch_assoc($page_set)) {
+          ?>
           <tr>
               <td><?php echo h($page['id']); ?></td>
+              <td><?php echo h($page['subject_id']); ?></td>
               <td><?php echo h($page['position']); ?></td>
               <td><?php echo $page['visible'] == 1 ? 'true' : 'false'; ?></td>
               <td><?php echo h($page['menu_name']); ?></td>
@@ -43,6 +51,8 @@
           </tr>
           <?php } ?>
         </table>
+
+        <?php mysqli_free_result($page_set); ?>
 
     </div>
   </div>
