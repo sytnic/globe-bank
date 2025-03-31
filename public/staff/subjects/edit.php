950c7ce5611d1f7cede0184ba4c7ad17fb8525ac
sytnic <sytnic@list.ru>
2025-03-31 16:10:42 +0300

    41.1 Use form data to update records. SQL query

--- public/staff/subjects/edit.php
+++ public/staff/subjects/edit.php
@@ -8,27 +8,41 @@ require_once('../../../private/initialize.php');
 if(!isset($_GET['id'])) {
   redirect_to(url_for('/staff/subjects/index.php'));
 }
+
 $id = $_GET['id'];
-
-$subject = find_subject_by_id($id);
-
-$menu_name = '';
-$position = '';
-$visible = '';
 
 // Если была post-отправка, то обработка значений
 if (is_post_request()) {
   // Handle form values sent by new.php
+
+  $subject = [];
   
-      $menu_name = $_POST['menu_name'] ?? '';
-      $position = $_POST['position'] ?? '';
-      $visible = $_POST['visible'] ?? '';
+  $subject['menu_name'] = $_POST['menu_name'] ?? '';
+  $subject['position'] = $_POST['position'] ?? '';
+  $subject['visible'] = $_POST['visible'] ?? '';
   
-      echo "Form parameters<br />";
-      echo "Menu name: " . $menu_name . "<br />";
-      echo "Position: " . $position . "<br />";
-      echo "Visible: " . $visible . "<br />";
+  $sql = "UPDATE subjects SET";
+  $sql.= " menu_name='".$subject['menu_name'] ."',";
+  $sql.= " position='".$subject['position'] ."',";
+  $sql.= " visible='".$subject['visible'] ."'";
+  $sql.= " WHERE id='".$id."' ";
+  $sql.= " LIMIT 1";
+
+  $result = mysqli_query($db, $sql);
+  // For UPDATE $result is true/false
+  if($result) {
+    redirect_to(url_for('/staff/subjects/show.php?id='.$id));
+  } else {
+    // Update failed
+    echo mysqli_error($db);
+    db_dissonnect($db);
+    exit;
   }
+  
+} else {
+
+  $subject = find_subject_by_id($id);
+}
 
 ?>
 
