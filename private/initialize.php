0a848ba07bef37092f2cfc31a5e5b73889a9e34e
sytnic <sytnic@list.ru>
2025-04-15 13:44:32 +0300

    46. Common data validation types

--- private/initialize.php
+++ private/initialize.php
@@ -54,6 +54,7 @@ define("WWW_ROOT", $doc_root);
 require_once('functions.php');
 require_once('database.php');
 require_once('query_functions.php');
+require_once('validation_functions.php');
 
 // вызвана функция из database.php
 // и установлено соединение с БД
